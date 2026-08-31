14db6a8e9e783724ac61c8f6203ee87fd2d700c3
hob21081973-glitch <hob21081973@gmail.com>
2026-08-30 18:43:39 -0600

    Delete android directory

--- android/app/src/main/kotlin/com/example/app_ventas_export_pdf/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.app_ventas_export_pdf
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/ventas/app/app_ventas_export_pdf/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ventas.app.app_ventas_export_pdf
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
