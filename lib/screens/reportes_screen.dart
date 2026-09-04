9b2ab0ffc11f79b528626fe2bdaf464969a771ea
hob21081973-glitch <hob21081973@gmail.com>
2026-09-03 22:48:44 -0600

    Update reportes_screen.dart

--- lib/screens/reportes_screen.dart
+++ lib/screens/reportes_screen.dart
@@ -1,6 +1,4 @@
 import 'package:app_ventas_export_pdf/pdf_helper.dart';
-// REEMPLAZA 'tu_app' por el nombre exacto de tu proyecto que figura en tu pubspec.yaml
-import 'package:tu_app/helpers/pdf_helper.dart'; 
 
 class ReportesScreen extends StatefulWidget {
   const ReportesScreen({Key? key}) : super(key: key);
