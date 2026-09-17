027b2a820b7145a5bc86d208bd807ab8f557ae43
hob21081973-glitch <hob21081973@gmail.com>
2026-09-16 20:41:27 -0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/resumen_pedido/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/resumen_pedido/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.resumen_pedido
+package com.example.app_ventas_exportpdf
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
